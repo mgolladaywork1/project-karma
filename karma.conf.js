c928bc4c1ec0570f9d7db08d66fafc42d5962bab
Jeff Weyer <jweyer@snap-software.com>
2018-04-21 22:08:47 -0700

    Added configuration to remove compaction of source when using coverage.

--- karma.conf.js
+++ karma.conf.js
@@ -39,11 +39,18 @@ module.exports = function (config) {
       variableName: '__json__'
     },
 
+
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress', 'coverage'],
 
+
+    coverageReporter: {
+      instrumenterOptions: {
+        istanbul: { noCompact: true }
+      }
+    },
 
     // web server port
     port: 9876,
